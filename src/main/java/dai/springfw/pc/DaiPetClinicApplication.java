1ee2af5755db994c8c4d088631ab430d265130e4
DaiGithb <xnben@xnben.org>
2020-09-25 20:25:21 -0400

    Initial Project Add

---
+++ src/main/java/dai/springfw/pc/DaiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package dai.springfw.pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaiPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaiPetClinicApplication.class, args);
+    }
+
+}
