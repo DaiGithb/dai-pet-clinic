8267bee27c33bd08872ea1a5c31026e6dc2746dc
DaiGithb <xnben@xnben.org>
2020-09-26 17:34:06 -0400

    added POJOs. Close #1 issue.

---
+++ src/main/java/dai/springfw/pc/model/Owner.java
@@ -0,0 +1,4 @@
+package dai.springfw.pc.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/dai/springfw/pc/model/Vet.java
@@ -0,0 +1,6 @@
+package dai.springfw.pc.model;
+
+import dai.springfw.pc.model.Person;
+
+public class Vet extends Person {
+}
