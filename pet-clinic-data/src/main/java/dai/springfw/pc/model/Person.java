8a5cff7de9ab1680f022284374e1529af57d0d57
DaiGithb <xnben@xnben.org>
2020-09-26 22:42:13 -0400

    Added BaseEntity and ID. close #7

--- pet-clinic-data/src/main/java/dai/springfw/pc/model/Person.java
+++ pet-clinic-data/src/main/java/dai/springfw/pc/model/Person.java
@@ -1,6 +1,7 @@
 package dai.springfw.pc.model;
 
-public class Person {
+public class Person extends BaseEntity {
+
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/dai/springfw/pc/model/PetType.java
+++ pet-clinic-data/src/main/java/dai/springfw/pc/model/PetType.java
@@ -1,6 +1,6 @@
 package dai.springfw.pc.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
