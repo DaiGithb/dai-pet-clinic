26518786b9799e52ee4cee18d66323d4b676a544
DaiGithb <xnben@xnben.org>
2020-09-27 09:57:52 -0400

    Implemented Map based service. close #5

---
+++ pet-clinic-data/src/main/java/dai/springfw/pc/services/Map/AbstractMapService.java
@@ -0,0 +1,31 @@
+package dai.springfw.pc.services.Map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> {
+
+    protected Map<ID, T> map = new HashMap<>();
+
+    Set<T> findAll() {
+        return new HashSet<>(map.values());
+    }
+
+    T findById(ID id) {
+        return map.get(id);
+    }
+
+    T save(ID id, T obj) {
+        return map.put(id, obj);
+    }
+
+    void deleteById(ID id) {
+         map.remove(id);
+    }
+
+    void delete(T obj) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(obj));
+    }
+}
