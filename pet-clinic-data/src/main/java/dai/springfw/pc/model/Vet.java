024c2999c39b3f301d2aaa56765e177b0e9c07a4
DaiGithb <xnben@xnben.org>
2020-09-28 15:50:25 -0400

    Created Visit and Speciality POJOs and updated others. close #24

--- pet-clinic-data/src/main/java/dai/springfw/pc/model/Vet.java
+++ pet-clinic-data/src/main/java/dai/springfw/pc/model/Vet.java
@@ -2,5 +2,17 @@ package dai.springfw.pc.model;
 
 import dai.springfw.pc.model.Person;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> speciality;
+
+    public Set<Speciality> getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Set<Speciality> speciality) {
+        this.speciality = speciality;
+    }
 }
