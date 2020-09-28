772916cd6afca05ea46d0d966f90637d61f553a1
DaiGithb <xnben@xnben.org>
2020-09-27 20:19:32 -0400

    Added Owner list to Model and updated controller. close #15

--- pet-clinic-web/src/main/java/dai/springfw/pc/controllers/VetController.java
+++ pet-clinic-web/src/main/java/dai/springfw/pc/controllers/VetController.java
@@ -1,14 +1,23 @@
 package dai.springfw.pc.controllers;
 
+import dai.springfw.pc.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index"})
-    public String listVets(){
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","/vets/index"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findALL());
         return "vets/index";
     }
 
