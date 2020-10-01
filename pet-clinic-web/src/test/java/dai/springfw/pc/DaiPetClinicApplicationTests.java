c031d4d3b2e850237e452d544026bd238dcae538
DaiGithb <xnben@xnben.org>
2020-09-30 22:16:05 -0400

    Wrote CRUD Tests for Owner JPA Service. close #41

--- pet-clinic-web/src/test/java/dai/springfw/pc/DaiPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/dai/springfw/pc/DaiPetClinicApplicationTests.java
@@ -1,12 +1,16 @@
 package dai.springfw.pc;
 
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class DaiPetClinicApplicationTests {
 
