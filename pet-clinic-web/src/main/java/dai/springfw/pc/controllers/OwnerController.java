be0b7ee2afe26c5da8f5f83b1f061e3bc5bfbf4d
DaiGithb <xnben@xnben.org>
2020-10-02 12:35:36 -0400

    Displayed Owner Record. close #43

--- pet-clinic-web/src/main/java/dai/springfw/pc/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/dai/springfw/pc/controllers/OwnerController.java
@@ -1,23 +1,38 @@
 package dai.springfw.pc.controllers;
 
+import dai.springfw.pc.model.Owner;
+import dai.springfw.pc.model.Pet;
+import dai.springfw.pc.repos.OwnerRepo;
+import dai.springfw.pc.repos.VisitRepo;
 import dai.springfw.pc.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Set;
 
 @Controller
 public class OwnerController {
 
     private final OwnerService ownerService;
+    private final OwnerRepo owners;
+    private VisitRepo visits;
 
-    public OwnerController(OwnerService ownerService) {
+    public OwnerController(OwnerService ownerService, OwnerRepo owners, VisitRepo visits) {
         this.ownerService = ownerService;
+        this.owners = owners;
+        this.visits = visits;
     }
 
     @RequestMapping({"/owners","/owners/index","/owners/index.html"})
     public String listOwners(Model model){
 
-        model.addAttribute("owners", ownerService.findALL());
+        Set<Owner> owners =  ownerService.findALL();
+        model.addAttribute("owners", owners);
+        //model.addAttribute("owners", ownerService.findALL());
 
         return "owners/index";
     }
@@ -25,6 +40,19 @@ public class OwnerController {
     @RequestMapping("/owners/find")
     public String findOwners(){
         return "owners/index";
+    }
+
+
+    @GetMapping("/owners/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+//        Owner owner = this.owners.findById(ownerId);
+//        for (Pet pet : owner.getPets()) {
+//            pet.setVisitsInternal(visits.findByPetId(pet.getId()));
+//        }
+//        mav.addObject(owner);
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 
 }
